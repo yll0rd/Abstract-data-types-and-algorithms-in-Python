8a1c90a4b86ed7aca04fa78d91d6549b9e4b3f34
LeoYoumbi <Leoyoumbi83@gmail.com>
2023-02-10 07:20:31 +0100

    "Creation of checkout.py in Abstract Data types"

---
+++ Abstract Data Types/checkdates.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+def main():
+    bornBefore = date(2006,1,14)
+    print(bornBefore)
+    Date = promptandExactDate()
+    while Date != None:
+        if Date <= bornBefore:
+            print("Is at least 17 of age: ", Date)
+        Date = promptandExactDate()
+
+def promptandExactDate():
+    print("Enter a birth date.")
+    month = int(input("month (0 to quit): "))
+    if month == 0 :
+        return None
+    else :
+        day = int( input("day: ") )
+        year = int( input("year: ") )
+        return date(year,month,day)
+
+main()
