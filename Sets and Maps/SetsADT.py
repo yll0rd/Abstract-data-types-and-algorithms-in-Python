b13a3fcaefe21e82e1052df1579d38eb192637a5
LeoYoumbi <Leoyoumbi83@gmail.com>
2023-02-17 02:44:51 +0100

    ''

--- Sets and Maps/SetsADT.py
+++ Sets and Maps/SetsADT.py
@@ -16,6 +16,37 @@ class Set:
         assert element in self, "The element must be in the set"
         self.Elements.remove(element)
 
+    def equal(self, otherSet):
+        if len(otherSet.Elements) != len(self.Elements): return False
+        else: return self.isSubsetOf(otherSet)
+
+    def isSubsetOf(self, otherSet):
+        for element in self:
+            if element not in otherSet: return False
+            return True
+
+    def union(self, otherSet):
+        newSet = Set()
+        newSet.Elements.extend(self.Elements)
+        for element in otherSet:
+            if element not in self:
+                newSet.add(element)
+        return newSet
+
+    def intersect(self, otherSet):
+        newSet = Set()
+        for element in self:
+            if element in otherSet:
+                newSet.add(element)
+        return newSet
+
+    def difference(self, otherSet):
+        newSet = Set()
+        newSet.Elements.extend(self.Elements)
+        for element in otherSet:
+            if element in self:
+                newSet.remove(element)
+
 if __name__ == '__main__':
     leo = Set()
     leo.add(1)
