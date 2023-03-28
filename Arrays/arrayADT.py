c215b6a5fcbb70ddf20ebf9c88ea887c4998eb64
LeoYoumbi <Leoyoumbi83@gmail.com>
2023-03-28 09:00:19 +0100

    Fix: self.__getitem__() in arrayADT implementation

--- Arrays/arrayADT.py
+++ Arrays/arrayADT.py
@@ -1,5 +1,6 @@
 import ctypes
 
+
 class Array:
     def __init__(self, size):
         assert size > 0, "Array size must be > 0"
@@ -12,13 +13,13 @@ class Array:
 
     def __len__(self):
         return self.size
-    
-    def getItem(self, index):
-        assert index >= 0 and index < self.__len__(), "Array subscript out of range"
+
+    def __getitem__(self, index):
+        assert 0 <= index < self.__len__(), "Array subscript out of range"
         return self.elements[index]
-    
+
     def setItem(self, index, value):
-        assert index >= 0 and index < self.__len__(), "Array subscript out of range"
+        assert 0 <= index < self.__len__(), "Array subscript out of range"
         self.elements[index] = value
 
     def clear(self, value):
@@ -35,9 +36,10 @@ class Array:
                 break
         return snap
 
-
     def __iter__(self):
         return ArrayIterator(self.elements)
+
+
 class ArrayIterator:
     def __init__(self, theArray):
         self.arr = theArray
@@ -45,7 +47,7 @@ class ArrayIterator:
 
     def __iter__(self):
         return self
-    
+
     def __next__(self):
         if self.index < len(self.arr) and self.arr[self.index] is not None:
             entry = self.arr[self.index]
@@ -53,14 +55,11 @@ class ArrayIterator:
             return entry
         else:
             raise StopIteration
-    
-
-
 
 
 # Implementation of the Array2D ADT using an array of arrays
 class Array2D:
-    def __init__(self, numRows, numCols): 
+    def __init__(self, numRows, numCols):
         # Create a 1-D array to store an array reference for each row.
         self.theRows = Array(numRows)
         # Create the 1-D arrays for each row of the 2-D array.
@@ -69,47 +68,48 @@ class Array2D:
 
     def getnumRows(self):
         return self.theRows.__len__()
-    
+
     def isEmpty(self):
         for row in range(self.getnumRows()):
-            theArray = self.theRows.getItem(row)
+            theArray = self.theRows.__getitem__(row)
             for col in range(self.getnumCols()):
-                if theArray.getItem(col) != None:
+                if theArray.__getitem__(col) != None:
                     return False
         return True
-    
+
     def getnumCols(self):
-        n = self.theRows.getItem(0)
+        n = self.theRows.__getitem__(0)
         return n.__len__()
-    
+
     def clear(self, value):
         for row in range(self.getnumRows()):
-            theArray = self.theRows.getItem(row)
+            theArray = self.theRows.__getitem__(row)
             for col in range(self.getnumCols()):
                 theArray.setItem(col, value)
-        
+
     def __getItem__(self, indexTuple):
         assert len(indexTuple) == 2, "Invalid number of array subscripts."
         row = indexTuple[0]
         col = indexTuple[1]
-        assert row >= 0 and row < self.getnumRows() \
-        and col >= 0 and col < self.getnumCols(), \
-        "Array subscript out of range."
-        theArray = self.theRows.getItem(row)
-        return theArray.getItem(col)
-    
+        assert 0 <= row < self.getnumRows() \
+               and 0 <= col < self.getnumCols(), \
+            "Array subscript out of range."
+        theArray = self.theRows.__getitem__(row)
+        return theArray.__getitem__(col)
+
     def __setItem__(self, indexTuple, value):
         assert len(indexTuple) == 2, "Invalid number of array subscripts."
         row = indexTuple[0]
         col = indexTuple[1]
-        assert row >= 0 and row < self.getnumRows() \
-        and col >= 0 and col < self.getnumCols(), \
-        "Array subscript out of range."
-        theArray = self.theRows.getItem(row)
-        theArray.setItem(col,value)
+        assert 0 <= row < self.getnumRows() \
+               and 0 <= col < self.getnumCols(), \
+            "Array subscript out of range."
+        theArray = self.theRows.__getitem__(row)
+        theArray.setItem(col, value)
+
 
 if __name__ == "__main__":
-    a = Array2D(2,3)
+    a = Array2D(2, 3)
     a.clear(None)
     if not a.isEmpty():
         for row in range(a.getnumRows()):
@@ -120,3 +120,8 @@ if __name__ == "__main__":
     for row in range(a.getnumRows()):
         for col in range(a.getnumCols()):
             print(f"A[{row}][{col}]: {a.__getItem__((row, col))}")
+
+    A = Array(4)
+    for i in range(4):
+        A.setItem(i, i * 2)
+    print(A[1])
